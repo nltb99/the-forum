43c3d89a61119e8e0f7df011445d5d78cf9399ff
Your Name <you@example.com>
2020-08-02 18:11:33 +0700

    Adjust switch mode

--- controllers/redis/redisClient.js
+++ controllers/redis/redisClient.js
@@ -1,11 +1,14 @@
 const redis = require('redis')
 const url = require('url')
 // const redisURL = url.parse(process.env.REDISCLOUD_URL)
-const REDIS_PORT = process.env.REDISCLOUD_URL || 6379
 
-const client = redis.createClient(REDIS_PORT)
+// const REDIS_PORT = redisURL.port || 6379
+// const REDIS_HOSTNAME = redisURL.hostname || '127.0.0.1'
 
-// const client = redis.createClient(redisURL.port, redisURL.hostname, { no_ready_check: true })
-// client.auth(redisURL.auth.split(':')[1])
+const client = redis.createClient(process.env.REDISCLOUD_URL || 6379, { no_ready_check: true })
 
+// const client = redis.createClient(REDIS_PORT)
+// client.auth(redisURL.auth.split(':')[1] || null)
+
+// const client = redis.createClient(process.env.REDISCLOUD_URL, { no_ready_check: true })
 module.exports = client
