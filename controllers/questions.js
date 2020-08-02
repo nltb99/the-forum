9878ece6345a684a0a2d32fd32b6b82bf6f6608c
Your Name <you@example.com>
2020-08-02 20:58:01 +0700

    Fix 500

--- controllers/questions.js
+++ controllers/questions.js
@@ -4,23 +4,11 @@ const QuestionSchema = require('../model/questionSchema.js')
 
 const authToken = require('./verifyToken/token')
 
-const client = require('./redis/redisClient')
+// const client = require('./redis/redisClient')
 
 // Cache middleware
-function cacheAllQuestion(req, res, next) {
-    client.get('fetchAllQuestions', (err, data) => {
-        if (err) throw err
-        if (data !== null) {
-            res.json(JSON.parse(data))
-        } else {
-            next()
-        }
-    })
-}
-
-// function cacheSpecificQuestion(req, res, next) {
-//     const { id } = req.params
-//     client.get(id, (err, data) => {
+// function cacheAllQuestion(req, res, next) {
+//     client.get('fetchAllQuestions', (err, data) => {
 //         if (err) throw err
 //         if (data !== null) {
 //             res.json(JSON.parse(data))
@@ -33,12 +21,12 @@ function cacheAllQuestion(req, res, next) {
 /////////////////////////////////
 
 // Handle question route
-route.get('/question', cacheAllQuestion, async (req, res) => {
+route.get('/question', async (req, res) => {
     try {
         const questions = await QuestionSchema.find({}).sort({ createAt: -1 })
 
         // Set data to Redis
-        client.setex('fetchAllQuestions', 2000, JSON.stringify(questions))
+        // client.setex('fetchAllQuestions', 2000, JSON.stringify(questions))
         res.json(questions)
     } catch (e) {
         console.log(e)
@@ -49,9 +37,6 @@ route.get('/question/:id', async (req, res) => {
     try {
         const { id } = req.params
         const question = await QuestionSchema.findById(req.params.id)
-
-        // Set data to Redis
-        // client.setex(id, 2000, JSON.stringify(question))
         res.json(question)
     } catch (e) {
         console.log(e)
@@ -79,12 +64,12 @@ route.post('/question', async (req, res) => {
         await schema.save()
 
         // Update fetchAllQuestions
-        await client.get('fetchAllQuestions', (err, data) => {
-            if (err) throw err
-            let parsedData = JSON.parse(data)
-            parsedData.push(schema)
-            client.setex('fetchAllQuestions', 2000, JSON.stringify(parsedData))
-        })
+        // await client.get('fetchAllQuestions', (err, data) => {
+        //     if (err) throw err
+        //     let parsedData = JSON.parse(data)
+        //     parsedData.push(schema)
+        //     client.setex('fetchAllQuestions', 2000, JSON.stringify(parsedData))
+        // })
 
         res.send('success')
     } catch (e) {
@@ -96,14 +81,15 @@ route.post('/question', async (req, res) => {
 route.delete('/question/:id', checkMatchId, async (req, res) => {
     try {
         await req.question.remove()
+
         // Delete question in redis
-        const { id } = await req.params
-        await client.get('fetchAllQuestions', (err, data) => {
-            if (err) throw err
-            let parsedData = JSON.parse(data)
-            let newParsedDataArray = parsedData.filter((data) => data._id !== id)
-            client.setex('fetchAllQuestions', 2000, JSON.stringify(newParsedDataArray))
-        })
+        // const { id } = await req.params
+        // await client.get('fetchAllQuestions', (err, data) => {
+        //     if (err) throw err
+        //     let parsedData = JSON.parse(data)
+        //     let newParsedDataArray = parsedData.filter((data) => data._id !== id)
+        //     client.setex('fetchAllQuestions', 2000, JSON.stringify(newParsedDataArray))
+        // })
 
         return res.send('delete successed')
     } catch (e) {
