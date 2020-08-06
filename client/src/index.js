d4ce70e7b1850325ab1d8ef9f3c67ff216e191bd
Your Name <you@example.com>
2020-08-06 12:43:10 +0700

    Ver 2.0.0

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import { SWRConfig } from 'swr';
 import axios from 'axios';
 import { getCookie } from './redux/actions/actionTypes';
 
-axios.defaults.baseURL = 'https://localhost:5000';
+// axios.defaults.baseURL = 'https://localhost:5000';
 axios.defaults.headers.common['Authorization'] = `Bearer ${getCookie('tk')}`;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
